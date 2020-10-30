94977e49aa4ee03b9584eb72d87df72b7b907b46
Renuja De Costa <devmindadeco@gmail.com>
2020-10-30 15:40:34 +0530

    Product details screen

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -34,7 +34,7 @@ const Rating = ({value, text, color}) => {
                 <i style ={{color}} className={ value >= 5 ? 'fas fa-star': value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
 
-            <div>{ text && text}</div>
+            <span>{ text && text}</span>
         </div>
     )
 }
--- frontend/src/products.js
+++ frontend/src/products.js
@@ -8,9 +8,9 @@ const products = [
     brand: 'Apple',
     category: 'Electronics',
     price: 89.99,
-    countInStock: 10,
+    countInStock: 0,
     rating: 4.5,
-    numReviews: 12,
+    numReviews: 5,
   },
   {
     _id: '2',
@@ -36,7 +36,7 @@ const products = [
     price: 929.99,
     countInStock: 5,
     rating: 3,
-    numReviews: 12,
+    numReviews: 7,
   },
   {
     _id: '4',
@@ -49,7 +49,7 @@ const products = [
     price: 399.99,
     countInStock: 11,
     rating: 5,
-    numReviews: 12,
+    numReviews: 9,
   },
   {
     _id: '5',
@@ -62,7 +62,7 @@ const products = [
     price: 49.99,
     countInStock: 7,
     rating: 3.5,
-    numReviews: 10,
+    numReviews: 5,
   },
   {
     _id: '6',
@@ -75,7 +75,7 @@ const products = [
     price: 29.99,
     countInStock: 0,
     rating: 4,
-    numReviews: 12,
+    numReviews: 6,
   },
 ]
 
