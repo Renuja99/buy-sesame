bcf3e5288ba6298a7c160763638344560ee3672c
Renuja De Costa <devmindadeco@gmail.com>
2020-10-30 10:45:21 +0530

    Homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './bootstrap.min.css';
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <div className="App">
-     <h1>Welcome to buy_sesame</h1>
+      <Header />
+      
+      
+      <main className='py-3' >
+      <Container>
+        <HomeScreen />
+      </Container>
+        
+      </main>
+      <Footer />
     </div>
+
   );
 }
 
