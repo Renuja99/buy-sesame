56bd75cde367f085d4c62cf4034e77620c599332
Renuja De Costa <devmindadeco@gmail.com>
2020-11-06 16:37:06 +0530

    Product Details Reducer & Action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, {  useEffect  } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 
@@ -23,7 +25,8 @@ const HomeScreen = () => {
     return (
         <>
           <h1>Latest products</h1>
-    {loading ? ( <h2>Loading...</h2>) : error ? <h3>{error}</h3>:  <Row>
+    {loading ? ( <Loader />) : error ? <Message variant = 'danger'>{error}</Message>:  
+          <Row>
               {products.map((product) =>(
                   <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,49 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch , useSelector} from 'react-redux'
 import { Row , Col, Image , ListGroup , Card , Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import {listProductDetails } from '../actions/productActions'
+
 
 const ProductScreen = ({match}) => {
 
-    const [product, setProduct] =useState({})
+
+    const dispatch = useDispatch()
+
+    const productDetails = useSelector( state => state.productDetails)
+
+    const { loading , error , product} = productDetails
 
     useEffect(() =>{
+        dispatch(listProductDetails(match.params.id))
+     
+      },[dispatch,match])
 
-        const fetchProduct = async () => {
-  
-          const {data} = await axios.get(`/api/products/${match.params.id}`);
-  
-          setProduct(data)
-        }
-  
-        fetchProduct();
-      },[match])
     
     return (
         <>
         <Link className = 'btn btn-light my-3' to='/'> Go Back </Link>
-        <Row>
-            <Col md={6}>
-                <Image src = {product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={3}>
-                <ListGroup variant ='flush'>
-                    <ListGroup.Item>
-                        <h3>{product.name}</h3>
-                    </ListGroup.Item>
-                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-                    <ListGroup.Item>
-                        Price : ${product.price}
-                    </ListGroup.Item>
-                    <ListGroup>
-                        Description: {product.description}
-                    </ListGroup>
+    {loading ? <Loader/>: error ? <Message variant='danger'>{error}</Message>:(
 
-                    
-                </ListGroup>
-            </Col>
-            <Col md={3}>
+            <Row>
+              <Col md={3}>
                 <Card>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
@@ -73,8 +59,29 @@ const ProductScreen = ({match}) => {
                         </ListGroup.Item>
                     </ListGroup>
                 </Card>
+            </Col>      
+            <Col md={6}>
+                <Image src = {product.image} alt={product.name} fluid />
             </Col>
-        </Row>
+            <Col md={3}>
+                <ListGroup variant ='flush'>
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                    <ListGroup.Item>
+                        Price : ${product.price}
+                    </ListGroup.Item>
+                    <ListGroup>
+                        Description: {product.description}
+                    </ListGroup>
+
+                    
+                </ListGroup>
+            </Col>
+            </Row>
+    )}
+       
         </>
     )
 }
