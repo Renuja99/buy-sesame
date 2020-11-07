f7724cb0f4887d2a46c0da19d9c3f3b4fb44af4f
Renuja De Costa <devmindadeco@gmail.com>
2020-11-07 19:11:45 +0530

    Remove items from cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems:[]}, action) => {
 
@@ -22,6 +22,12 @@ export const cartReducer = (state = { cartItems:[]}, action) => {
                     ...state,
                     cartItems: [...state.cartItems, item]
                 }
+            }
+
+        case CART_REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: state.cartItems.filter((x)=> x.product !== action.payload)
             }
         default:
             return state
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector} from 'react-redux'
 import {Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart ,removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({match, location, history}) => {
     
@@ -25,13 +25,14 @@ const CartScreen = ({match, location, history}) => {
     }, [dispatch, productId, qty])
 
     const removeFromCartHandler = (id) => {
-        history.push('login?redirect=shipping')
+        
+        dispatch(removeFromCart(id))
     }
 
 
     const checkoutHandler = () =>{
 
-        console.log('checkout')
+        history.push('login?redirect=shipping')
     }
     return (
         <Row>
