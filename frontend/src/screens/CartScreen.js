a5a56b822c893e287510769b2166e76ca295100f
Renuja De Costa <devmindadeco@gmail.com>
2020-11-07 17:48:42 +0530

    Cart Screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -17,22 +17,82 @@ const CartScreen = ({match, location, history}) => {
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
 
-    console.log(cartItems);
-
     useEffect(()=>{
         if(productId){
 
             dispatch(addToCart(productId, qty))
         }
     }, [dispatch, productId, qty])
+
+    const removeFromCartHandler = (id) => {
+        history.push('login?redirect=shipping')
+    }
+
+
+    const checkoutHandler = () =>{
+
+        console.log('checkout')
+    }
     return (
-        <div>
-            Cart 
-        </div>
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 ? <Message>Your cart is empty <Link to='/'>Go back </Link>
+                </Message>: (
+                    <ListGroup variant ='flush'>
+                        {cartItems.map(item =>(
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded />
+                                    </Col>
+                                    <Col md={3}>
+                                        <Link to={`/product/$(item.product)`}>{item.name}</Link>
+                                    </Col>
+                        <Col md={2}> ${item.price}</Col>
+                        <Col md={2}>
+                    <Form.Control as='select' value={item.qty} onChange={(e)=> dispatch(addToCart(item.product, Number(e.target.value)))}>
+                        {
+                            [...Array(item.countInStock).keys()].map(x => (
+                                <option key={x+1} value={x+1}>
+                                    {x+1}
+                                </option>
+                            ))
+                        }
+                    </Form.Control>
+                        </Col>
+                        <Col md={2}>
+                            <Button type='button' variant='light' onClick={()=>removeFromCartHandler(item.product)}>
+                                <i className='fas fa-trash'></i>
+                            </Button>
+                        </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
+            </Col>
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h4><strong>Subtotal ({cartItems.reduce((acc, item)=>acc + item.qty,0) }) items</strong></h4>
+                            ${cartItems.reduce((acc, item)=> acc+ item.qty * item.price, 0 ).toFixed(2)}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                        <Button type='button' className= 'btn-block' disabled={cartItems.length ===0 } onClick={checkoutHandler}>
+                            Proceed to Checkout
+                        </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
     )
 }
 
 export default CartScreen
 
 // useEffect is where we want to dispatch addToCart
-// We bring in useDispatch and useSelect because we are dealing with our redux state
+// We bring in useDispatch and useSelector because we are dealing with our redux state
+//useSelector is used to display data from the Redux store. Can be demonstrated using console.log(useSelector: cart)
