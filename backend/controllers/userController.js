7a5b9f461cccae1e767341e7e820cf367b297f06
Renuja De Costa <devmindadeco@gmail.com>
2020-11-11 12:03:07 +0530

    Update Profile Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -93,5 +93,39 @@ const getUserProfile = asyncHandler(async(req,res)=>{
    }
 })
 
+// @desc   Get user profile
+// @route  PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async(req,res)=>{
+ 
+    const user = await User.findById(req.user._id)
+ 
+    if(user){
+ 
+      user.name= req.body.name || user.name
+      user.email = req.body.email || user.email
+      if(req.body.password){
+          user.password = req.body.password
 
-export { authUser, getUserProfile , registerUser}
+      }
+
+      const updatedUser = await user.save()
+
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email:updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id),
+    })
+ 
+    } else{
+ 
+     res.status(401)
+     throw new Error('Invalid email or password')
+    }
+ })
+ 
+
+
+export { authUser, getUserProfile , registerUser, updateUserProfile}
